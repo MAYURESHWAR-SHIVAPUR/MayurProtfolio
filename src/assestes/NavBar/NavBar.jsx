8e8265690ce8e0e0da4e744ba274eb1e67315fa0
Mayureshwar S <mayur20032004@gmail.com>
2025-07-15 10:14:48 +0530

    no animation only navbar updated

--- src/assestes/NavBar/NavBar.jsx
+++ src/assestes/NavBar/NavBar.jsx
@@ -1,8 +1,22 @@
 import React, { useRef, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 import NavCss from './NavBar.module.css';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+
 
 const NavBar = () => {
+  useGSAP(() => {
+    gsap.from("section .a", {
+      y: -50,
+      opacity: 0,
+      duration: 2,
+      delay: 6,
+      stagger:1,
+    })
+  }, []);
+
+
   const section = useRef();
   let visible = true;
 
@@ -46,11 +60,11 @@ const NavBar = () => {
         <i className="fa-solid fa-bars"></i>
       </button>
       <section ref={section} className={NavCss.navSection}>
-        <NavLink className={({ isActive }) => isActive ? NavCss.Active : ""} to='/'>Home</NavLink>
-        <NavLink className={({ isActive }) => isActive ? NavCss.Active : ""} to='/about'>About</NavLink>
-        <NavLink className={({ isActive }) => isActive ? NavCss.Active : ""} to='/portfolio/project'>Portfolio</NavLink>
-        <NavLink className={({ isActive }) => isActive ? NavCss.Active : ""} to='/contact'>Contact</NavLink>
-        <NavLink className={({ isActive }) => isActive ? NavCss.Active : ""} to='/resume'>Resume</NavLink>
+        <NavLink className={({ isActive }) => isActive ? NavCss.Active : "a"} to='/'>Home</NavLink>
+        <NavLink className={({ isActive }) => isActive ? NavCss.Active : "a"} to='/about'>About</NavLink>
+        <NavLink className={({ isActive }) => isActive ? NavCss.Active : "a"} to='/portfolio/project'>Portfolio</NavLink>
+        <NavLink className={({ isActive }) => isActive ? NavCss.Active : "a"} to='/contact'>Contact</NavLink>
+        <NavLink className={({ isActive }) => isActive ? NavCss.Active : "a"} to='/resume'>Resume</NavLink>
       </section>
     </div>
   );
